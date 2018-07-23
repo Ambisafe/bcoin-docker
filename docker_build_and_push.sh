02fef6d12185e649d3153eaa0ab9a80247e3ddcc
Ihor Savchenko <ihor@ambisafe.co>
2018-07-23 23:11:17 +0300

    update to v1.0.2

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -2,7 +2,7 @@
 set -ex
 
 DOCKER_REPO=ambisafe1/bcoin-node
-VERSION=v1.0.0-beta.15
+VERSION=v1.0.2
 
 docker build --no-cache --build-arg 'BCOIN_VERSION='$VERSION -t $DOCKER_REPO:$VERSION .
 docker push $DOCKER_REPO:$VERSION
